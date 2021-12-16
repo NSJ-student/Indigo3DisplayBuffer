1d0622d9f8797cdb716f80099dbc1e4c7637a252
NSJ-student <ghdlghdl0518@hanmail.net>
2021-12-16 22:17:57 +0900

    ui 틀

--- Indigo3DisplayBuffer/MainWindow.xaml.cs
+++ Indigo3DisplayBuffer/MainWindow.xaml.cs
@@ -24,5 +24,62 @@ namespace Indigo3DisplayBuffer
         {
             InitializeComponent();
         }
+
+        private void btnOpenPar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpenBin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            displayBuffer_SizeChange();
+        }
+
+        private void StackPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            displayBuffer_SizeChange();
+        }
+
+
+        /**************************/
+        //     Panel
+        /**************************/
+
+        private void displayBuffer_SizeChange()
+        {
+            try
+            {
+                int real_width = Convert.ToInt32(lblDispWidth.Content);
+                int real_height = Convert.ToInt32(lblDispHeight.Content);
+
+                double width = (gridMain.ActualWidth) -
+                    (stackControl.Margin.Left + stackControl.Margin.Right + stackControl.ActualWidth) - 5.0;
+                double height = (gridMain.ActualHeight) -
+                    (stackConnect.Margin.Bottom + stackConnect.Margin.Top + stackConnect.ActualHeight);
+
+                double exp_height = width * real_height / real_width;
+                if (exp_height <= height)
+                {
+                    cvsDispBuff.Width = width;
+                    cvsDispBuff.Height = exp_height;
+                    return;
+                }
+
+                double exp_width = height * real_width / real_height;
+                if (exp_width <= width)
+                {
+                    cvsDispBuff.Width = exp_width;
+                    cvsDispBuff.Height = height;
+                    return;
+                }
+            }
+            catch { }
+        }
+
     }
 }
